25344fff23a5686c8358ad79c021d7ad15ed1620
Kushagra007 <kushagrasinghbora@gmail.com>
2025-09-24 17:06:39 +0530

    Add files via upload

---
+++ Sum_of_Square.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int printSquareSum(int n)
+{
+    if(n==0)
+    return 0;
+    else
+    return (n*n)+printSquareSum(n-1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<printSquareSum(n);
+    return 0;
+}
