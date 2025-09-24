720e8ea48e26f74f490a1bfa97c8ca38d6edeab7
Kushagra007 <kushagrasinghbora@gmail.com>
2025-09-24 19:10:55 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,15 +4,13 @@ using namespace std;
 void palindrome(string n, int start, int end)
 {
     if(start >= end){
-        cout << "Palindrome" << endl;
+        cout << "palindrome" << endl;
         return;
     }
-
     if(n[start] != n[end]){
-        cout << "Not Palindrome" << endl;
+        cout << "not palindrome" << endl;
         return;
     }
-
     palindrome(n, start + 1, end - 1); 
 }
 int main()
@@ -21,4 +19,6 @@ int main()
     cin >> n;
     palindrome(n, 0, n.size() - 1);
     return 0;
+    
 }
+
